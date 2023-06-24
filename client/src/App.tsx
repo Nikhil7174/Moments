426b0f933fc412c22e30b5837c2cc3e3ae5220dc
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-06-24 22:29:51 +0530

    ..

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,32 +4,11 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import jwt_decode from "jwt-decode";
 import Auth from "./components/Auth/Auth";
 import PostDetails from "./components/PostDetails/PostDetails";
 
 function App() {
   let a = 0;
-  const [promptLogin, setPromptLogin] = useState(false);
-  const googleSuccessResponse = (res: any) => {
-    var token = res.credential;
-    setPromptLogin(true);
-    var result = jwt_decode(res.credential);
-    console.log("token: ", token);
-    console.log("result: ", result);
-    // document.getElementById("signInDiv").hidden = true;
-  };
-  useEffect(() => {
-    google.accounts.id.initialize({
-      client_id:
-        "797847668640-m6hk2og01eor7qmr2iii2ml1sko9625q.apps.googleusercontent.com",
-      callback: googleSuccessResponse,
-    });
-    {
-      console.log(promptLogin);
-      promptLogin === false && google.accounts.id.prompt();
-    }
-  }, []);
 
   const user = JSON.parse(localStorage.getItem("profile"));
   return (
