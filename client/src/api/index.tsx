5fb6640a60fa1ad38d8a866c4eadd540600914a1
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-06-24 23:34:22 +0530

    ..

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://moments-api.onrender.com" });
 
 API.interceptors.request.use((req: any): any => {
   if (localStorage.getItem("profile")) {
